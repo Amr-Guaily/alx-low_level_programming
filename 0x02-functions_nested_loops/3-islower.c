03392cced95f61de892e849e414bc8cdb61b3805
Amr Guaily <amrguaily@gmail.com>
2023-03-24 08:17:17 -0700

    fix bugs

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,9 @@
 #include "main.h"
 /**
+ * _islower - Check Holberton
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
  */
 
